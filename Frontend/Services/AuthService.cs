a26d86822716cbd0ccb1d7d7c00aaf7f07a0b339
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-06-17 20:18:02 +0100

    started doing AuthService.cs & navBarMenu

---
+++ Frontend/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿namespace Frontend.Services;
+
+public class AuthService
+{
+    private bool _isAuthenticated = false;
+    private string _role = "";
+
+    public bool IsAuthenticated => _isAuthenticated;
+    public string Role => _role;
+
+    public void Login(string role)
+    {
+        _isAuthenticated = true;
+        _role = role;
+    }
+
+    public void Logout()
+    {
+        _isAuthenticated = false;
+        _role = "";
+    }
+}
