5a1e7f8038bda0fbd34d4b5ee07c40f82ea7e75f
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-06-17 18:14:56 +0100

    commit before frontend

--- Backend/Controllers/TalentsController.cs
+++ Backend/Controllers/TalentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Backend.Models;
 using BusinessLogic.Entities;
 using BusinessLogic.databaseContext;
 using Microsoft.AspNetCore.Mvc;
@@ -25,11 +26,57 @@ namespace Backend.Controllers
         }
 
         [HttpPost]
-        public ActionResult<Professional> CreateProfessional(Professional professional)
+        public ActionResult<ProfessionalModel> CreateProfessional(ProfessionalModel professionalModel)
         {
+            // Verifica se o usuário existe
+            var user = _dbContext.Users.FirstOrDefault(u => u.Userid == professionalModel.UserID);
+            if (user == null)
+            {
+                return BadRequest("User does not exist");
+            }
+
+            // Cria uma nova entidade Professional
+            var professional = new Professional 
+            { 
+                Userid = professionalModel.UserID, 
+                Name = professionalModel.Name,
+                Country = professionalModel.Country,
+                Email = professionalModel.Email,
+                Hourlyrate = professionalModel.HourlyRate,
+                Visibility = professionalModel.Visibility,
+                User = user
+            };
+
             _dbContext.Professionals.Add(professional);
             _dbContext.SaveChanges();
-            return CreatedAtAction(nameof(GetProfessionalById), new { id = professional.Professionalid }, professional);
+
+            // Cria as entidades ProfessionalSkill
+            foreach (var professionalSkillModel in professionalModel.ProfessionalSkills)
+            {
+                var skill = _dbContext.Skills.FirstOrDefault(s => s.Skillid == professionalSkillModel.SkillID);
+                if (skill == null)
+                {
+                    return BadRequest($"Skill with id {professionalSkillModel.SkillID} does not exist");
+                }
+
+                var professionalSkill = new ProfessionalSkill 
+                {
+                    Professionalid = professional.Professionalid,
+                    Skillid = professionalSkillModel.SkillID,
+                    Yearsexperience = professionalSkillModel.YearsExperience,
+                    Professional = professional,
+                    Skill = skill
+                };
+
+                _dbContext.ProfessionalSkills.Add(professionalSkill);
+            }
+
+            _dbContext.SaveChanges();
+
+            // Copia o ID do Professional gerado para o modelo
+            professionalModel.ProfessionalID = professional.Professionalid;
+    
+            return CreatedAtAction(nameof(GetProfessionalById), new { id = professionalModel.ProfessionalID }, professionalModel);
         }
 
         [HttpGet("{id}")]
